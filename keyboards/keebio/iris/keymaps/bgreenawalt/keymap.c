5767a8867a7ca785606ef7e6f55c10257ab4a8a8
bgreenawalt <bgreenawalt.dev@gmail.com>
2025-06-16 11:01:08 -0400

    added grave key to iris

--- keyboards/keebio/iris/keymaps/bgreenawalt/keymap.c
+++ keyboards/keebio/iris/keymaps/bgreenawalt/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
         KC_TRNS, KC_LPRN, KC_LCBR, KC_LBRC, KC_LT,   KC_BSLS,                   KC_SLSH, KC_GT,   KC_RBRC, KC_RCBR, KC_RPRN, KC_TRNS,
-        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_TRNS, KC_TRNS, KC_EQL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
+        QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_GRV,   KC_MINS, KC_TRNS, KC_TRNS, KC_EQL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(6)),
     [5] = LAYOUT(
         KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
